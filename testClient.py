4e4af16ad420c789e645575c51b4f476e21e0a6b
XirisXER <twice2541@gmail.com>
2020-03-07 16:09:06 +0700

    update:: disconnect when server forcefully close the connection

--- testClient.py
+++ testClient.py
@@ -11,30 +11,41 @@ class ClientThread(threading.Thread):
 
     def run(self):
         while True:
-            strIn = self.clientSocket.recv(2048)
-            print("<server>:", strIn.decode('utf-8'))
+            try:
+                strIn = self.clientSocket.recv(2048)
+                print("<server>:", strIn.decode('utf-8'))
+            except Exception as e:
+                print("<System>:",self.clientSocket.getpeername(),e)
+                break
+        self.stop()
 
     def send(self, strOut):
         self.clientSocket.send((strOut+'\n').encode('utf-8'))
     def stop(self):
+        stopConnection(self.clientSocket.getpeername()[0])
+        self.clientSocket.shutdown(socket.SHUT_RDWR)
+        self.clientSocket.close()
+        print("<System>: Thread stop")
         self._stop_event.set()
 
 
-def startConnection(ipServ, strOut):
-    SERV_PORT = 50000
-    addr = (ipServ, SERV_PORT)
+def startConnection(addserv, strOut):
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         clientSocket.settimeout(10)
-        clientSocket.connect(addr)
+        clientSocket.connect(addserv)
         clientSocket.settimeout(None)
-        print("<System>: Server connected: " + str(addr[0])+':' + str(addr[1]))
-        brokerList.append(ipServ)
-        threadGroup[ipServ] = ClientThread(clientSocket)
-        threadGroup[ipServ].start()
-        threadGroup[ipServ].send(strOut)
+        print("<System>: Server connected: " + str(addserv[0])+':' + str(addserv[1]))
+        brokerList.append(addserv[0])
+        threadGroup[addserv[0]] = ClientThread(clientSocket)
+        threadGroup[addserv[0]].start()
+        threadGroup[addserv[0]].send(strOut)
     except Exception as e:
-        print("<System>: Something's wrong with "+str(addr)+". Exception is "+str(e))
+        print("<System>: Something's wrong with "+str(addserv)+". Exception is "+str(e))
+
+def stopConnection(ipServ):
+    brokerList.remove(ipServ)
+    threadGroup.pop(ipServ)
 
 
 brokerList = list()
@@ -42,23 +53,24 @@ threadGroup = dict()
 while True:
     temp = input().split(' ')
     try:
-        ipServ = temp[1]
+        tempaddr = temp[1].split(':') # {servip,servport}
+        addserv = (tempaddr[0],int(tempaddr[1]))
         strOut = temp[0]+' '+" ".join(temp[2:])
         if temp[0].lower() == "sub" or temp[0].lower() == "subscribe":
-            if ipServ not in brokerList:
-                startConnection(ipServ, strOut)
+            if addserv[0] not in brokerList:
+                startConnection(addserv, strOut)
             else:
-                threadGroup[ipServ].send(strOut)
+                threadGroup[addserv[0]].send(strOut)
         elif temp[0].lower() == "pub" or temp[0].lower() == "publish":
-            if ipServ not in brokerList:
-                startConnection(ipServ, strOut)
+            if addserv[0] not in brokerList:
+                startConnection(addserv, strOut)
             else:
-                threadGroup[ipServ].send(strOut)
+                threadGroup[addserv[0]].send(strOut)
         elif temp[0].lower() == "unsub" or temp[0].lower() == "unsubsribe":
-            if ipServ not in brokerList:
+            if addserv[0] not in brokerList:
                 raise Exception('You need to subscribe before unsubsribe')
             else:
-                threadGroup[ipServ].send(strOut)
+                threadGroup[addserv[0]].send(strOut)
         else:
             raise Exception('Wrong input')
         print("<You>:", strOut)
